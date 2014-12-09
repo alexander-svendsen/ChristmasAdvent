5aebfbb523702bed0fd37c66d0e89765095c6124
alexandersvendsen@hotmail.no <alexandersvendsen@hotmail.no>
2014-12-09 20:14:06 +0100

    Completed part 9 in python

--- 9-calc/main.py
+++ 9-calc/main.py
@@ -4,22 +4,15 @@ from itertools import permutations
 possible = [int(''.join(p)) for p in permutations('0123456789', 3) if p[0] != '0']
 
 
-def unique(result, num1, num2):
-    
-	
+def is_zero_to_nine(full_string):
+    return ''.join(sorted(full_string)) == '0123456789'
+
 
 result = 99999
 for x in possible:
-	for y in possible:
-		sum = str(x + y)
-		if len(sum) == 4:
-			product_str = str(x) + str(y)
-			for i in sum:
-				if i in product_str:
-					break
-			else:
-				print x,y,sum
-				result = min(result, x, y)
-
+    for y in possible:
+        sum = str(x + y)
+        if is_zero_to_nine(sum + str(x) + str(y)):
+            result = min(result, x, y)
 
 print result

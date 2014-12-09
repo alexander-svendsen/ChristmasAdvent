d04146407143503454cc2433910d355f6c17471f
alexander svendsen <alexander.svendsen@bekk.no>
2014-12-09 17:32:47 +0100

    almost complete

--- 9-calc/main.py
+++ 9-calc/main.py
@@ -1,5 +1,24 @@
 from itertools import permutations
 
-possible = [int(''.join(p)) for p in permutations('123456789', 3)]
 
-print 'bs' in 'bus'
+possible = [int(''.join(p)) for p in permutations('0123456789', 3) if p[0] != '0']
+
+
+def unique(result, num1, num2):
+	
+
+result = 99999
+for x in possible:
+	for y in possible:
+		sum = str(x + y)
+		if len(sum) == 4:
+			product_str = str(x) + str(y)
+			for i in sum:
+				if i in product_str:
+					break
+			else:
+				print x,y,sum
+				result = min(result, x, y)
+
+
+print result

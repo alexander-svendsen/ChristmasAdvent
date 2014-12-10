fbf0e7b77db72fcdc6e8813965e852d0242f8ead
alexandersvendsen@hotmail.no <alexandersvendsen@hotmail.no>
2014-12-10 20:26:42 +0100

    fixed the indentation and syntax

--- 10-christmas_table/main.py
+++ 10-christmas_table/main.py
@@ -1,8 +1,8 @@
-people = range(1,1501)
+people = range(1, 1501)
 
 index = 0
-while(len(people) > 1):
-	index = (index + 1) % len(people)
-	del people[index]
+while len(people) > 1:
+    index = (index + 1) % len(people)
+    del people[index]
 
 print people[0]
